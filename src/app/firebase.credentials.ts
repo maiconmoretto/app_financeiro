de8ab3d6019a434539d9434f7e1658a82026e2f4
maiconmoretto <maiconmorettos@gmail.com>
2018-01-06 17:36:26 -0200

    removendo app credentials

--- src/app/firebase.credentials.ts
+++
@@ -1,7 +0,0 @@
-export const    FIREBASE_CREDENTIALS = {
-    authDomain: "app-controle-financeiro-14533.firebaseapp.com",
-    databaseURL: "https://app-controle-financeiro-14533.firebaseio.com",
-    projectId: "app-controle-financeiro-14533",
-    storageBucket: "app-controle-financeiro-14533.appspot.com",
-    messagingSenderId: "55375268344"
-}
