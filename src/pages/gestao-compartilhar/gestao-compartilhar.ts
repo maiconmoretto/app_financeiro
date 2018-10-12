38614333b0583e579cc5fd22bf6ff36b284ad7f1
maiconmoretto <maiconmorettos@gmail.com>
2018-10-12 19:49:26 -0300

    *criado separação de  convites enviados para convites recebidos

--- src/pages/gestao-compartilhar/gestao-compartilhar.ts
+++ src/pages/gestao-compartilhar/gestao-compartilhar.ts
@@ -21,100 +21,159 @@ import { EditConvitePage } from '../edit-convite/edit-convite';
 })
 export class GestaoCompartilharPage {
 
-  convites$: FirebaseListObservable<ShoppingItem[]>
+  convitesEnviados = []
+  convitesRecebidos = []
   emailUsuario;
   idUsuario;
- 
-    constructor(
-      public navCtrl: NavController,
-      public NavParams: NavParams,
-      private fdb: AngularFireDatabase,
-      private toastCtrl: ToastController,
-      private database: AngularFireDatabase,
-      private authService: AuthService,
-      private actionSheetCtrl: ActionSheetController) {
-  this.convites$ = this.database.list('compartilhamento/');
-  this.emailUsuario = this.authService.getCurrentUserEmail;
-  this.idUsuario = this.authService.currentUserId;
-}
 
-enviarConvite(email) {
-  if (email == undefined) {
-    alert('Preencha todos os campos!');
+  constructor(
+    public navCtrl: NavController,
+    public NavParams: NavParams,
+    private fdb: AngularFireDatabase,
+    private toastCtrl: ToastController,
+    private database: AngularFireDatabase,
+    private authService: AuthService,
+    private actionSheetCtrl: ActionSheetController) {
+    this.listConvitesEnviados();
+    this.listConvitesRecebidos();
+    this.emailUsuario = this.authService.getCurrentUserEmail;
+    this.idUsuario = this.authService.currentUserId;
+  }
 
-    let msg = this.toastCtrl.create({
-      message: 'preencha todos o email!',
+
+  listConvitesEnviados() {
+    let self = this;
+    this.database.list('/compartilhamento/', {
+      preserveSnapshot: true,
+      query: {
+        orderByChild: 'id_usuario',
+        equalTo: this.authService.currentUserId
+      }
+    })
+      .subscribe(snapshots => {
+        snapshots.forEach(snapshot => {
+          self.convitesEnviados.push(
+            {
+              aceto: snapshot.val().aceito,
+              email_destinatario: snapshot.val().email_destinatario,
+              id: snapshot.key
+            }
+          );
+        });
+      })
+  }
+
+  listConvitesRecebidos() {
+    let self = this;
+    this.database.list('/compartilhamento/', {
+      preserveSnapshot: true,
+      query: {
+        orderByChild: 'email_destinatario',
+        equalTo: this.authService.getCurrentUserEmail
+      }
+    })
+      .subscribe(snapshots => {
+        snapshots.forEach(snapshot => {
+          self.convitesRecebidos.push(
+            {
+              aceto: snapshot.val().aceito,
+              email_remetente: snapshot.val().email_remetente,
+              id: snapshot.key
+            }
+          );
+        });
+      })
+  }
+
+  aceitarConvite(itemId) {
+    alert('Convite aceito ' +itemId);
+    // let msg = this.toastCtrl.create({
+    //   message: 'Convite aceito ' +itemId,
+    //   duration: 3000,
+    //   position: 'top'
+    // });
+
+    // msg.onDidDismiss(() => {
+    //   // console.log('Dismissed toast');
+    // });
+    // return;
+  }
+
+
+
+  enviarConvite(email) {
+    if (email == undefined) {
+      alert('Preencha todos os campos!');
+
+      let msg = this.toastCtrl.create({
+        message: 'preencha todos o email!',
+        duration: 3000,
+        position: 'top'
+      });
+
+      msg.onDidDismiss(() => {
+        // console.log('Dismissed toast');
+      });
+      return;
+    }
+
+    this.fdb.list("/compartilhamento/").push({
+      email_destinatario: email,
+      id_usuario: this.authService.currentUserId,
+      email_remetente: this.authService.getCurrentUserEmail,
+      aceito: 'nao',
+    });
+
+    let toast = this.toastCtrl.create({
+      message: 'Adicionado gasto com sucesso!',
       duration: 3000,
       position: 'top'
     });
 
-    msg.onDidDismiss(() => {
+    toast.onDidDismiss(() => {
       // console.log('Dismissed toast');
     });
-    return;
+
+    toast.present();
+    // this.navCtrl.pop();
   }
 
-  this.fdb.list("/compartilhamento/").push({
-    email_destinatario: email,
-    id_usuario: this.authService.currentUserId,
-    email_remetente: this.authService.getCurrentUserEmail,
-    aceito: false,
-  });
+  selectItem(itemId) {
+    console.log(' idd -> ' + itemId)
+    //display a actionsheet
+    //1 - edit 
+    //2 - remove item
+    //3 - cancel selection
+    this.actionSheetCtrl.create({
+      title: '',
+      buttons: [
+        {
+          text: 'Editar',
+          handler: () => {
+            //send the item to edit item and pass key as parameter
+            this.navCtrl.push(EditConvitePage,
+              {
+                itemId: itemId
+              });
+          }
+        },
+        // {
+        //   text: 'Delete',
+        //   role: 'destructive',
+        //   handler: () => {
+        //     //delete the current item
+        //     this.convitesEnviados$.remove(itemId);
+        //   }
+        // },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+      ]
+    }).present();
 
-  let toast = this.toastCtrl.create({
-    message: 'Adicionado gasto com sucesso!',
-    duration: 3000,
-    position: 'top'
-  });
-
-  toast.onDidDismiss(() => {
-    // console.log('Dismissed toast');
-  });
-
-  toast.present();
-  // this.navCtrl.pop();
-}
-
-selectItem(convites: ShoppingItem) {
-  console.log('id ' + convites.$key);
-  //display a actionsheet
-  //1 - edit 
-  //2 - remove item
-  //3 - cancel selection
-  this.actionSheetCtrl.create({
-    title: '',
-    buttons: [
-      {
-        text: 'Editar',
-        handler: () => {
-          //send the item to edit item and pass key as parameter
-          this.navCtrl.push(EditConvitePage,
-            {
-              itemId: convites.$key
-            });
-        }
-      },
-      {
-        text: 'Delete',
-        role: 'destructive',
-        handler: () => {
-          //delete the current item
-          this.convites$.remove(convites.$key);
-        }
-      },
-      {
-        text: 'Cancelar',
-        role: 'cancel',
-        handler: () => {
-
-
-        }
-      },
-    ]
-
-
-  }).present();
-
-}
+  }
 
 }
